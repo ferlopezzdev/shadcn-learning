ba3aa1003488f6e780835db3643b265d464bb083
FerLopez <ferlopeztrdev@gmail.com>
2024-07-17 17:52:17 -0400

    learning shadcn/ui

--- src/app/dashboard/button/page.tsx
+++ src/app/dashboard/button/page.tsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
+import { Mail, Loader2, ChevronRight } from "lucide-react";
 
 export default function Page() {
   return (
     <div className="grid grid-cols-5 gap-2">
+      {/* Todas las variantes de los botones */}
       <Button>default</Button>
       <Button variant={"destructive"}>destructive</Button>
       <Button variant={"ghost"}>Ghost</Button>
@@ -12,8 +14,31 @@ export default function Page() {
       <Button variant={"outline"}>outline</Button>
       <Button variant={"secondary"}>secondary</Button>
       <Button disabled>disable</Button>
+
+      {/* Variante personalizada desde el ui/button.tsx */}
       <Button variant={"succes"}>success</Button>
+
+      {/* Los componentes de shadcn/ui actuan de forma natural como en el HTML */}
       <Button onClick={() => console.log("Hola Mundo")}>Click Me!</Button>
+
+      {/* Props personalizado desde el ui/button.tsx */}
+      <Button capitalize={false}>capitalize false</Button>
+
+      {/* Botón con icono */}
+      <Button variant={"outline"}>
+        <Mail className="h-4 w-4" /> Login with Email
+      </Button>
+
+      {/* Botón ícono */}
+      <Button variant={"outline"} size={"icon"}>
+        <ChevronRight className="h-4 w-4" />
+      </Button>
+
+      {/* Botón cargando */}
+      <Button>
+        <Loader2 className="mr-2 h-4-w4 animate-spin" />
+        Please wait
+      </Button>
     </div>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "capitalize inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -38,15 +38,16 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
+  capitalize?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, capitalize = true, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }), {'capitalize': capitalize})}
         ref={ref}
         {...props}
       />
